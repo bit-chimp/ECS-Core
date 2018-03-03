2c474013bbd92f8ceebdfe3f7b95b737aa98a3a5
Simon van der Weele <simon@rightbrain.com.mt>
2018-03-03 18:29:12 +0100

    - added SpriteComponent - Any missing ECS components now added when found on Unity GameObject

---
+++ src/Components/Sprite/Components/SpriteComponent.cs
@@ -0,0 +1,10 @@
+﻿using Entitas.CodeGeneration.Attributes;
+
+namespace btcp.ECS.src.Components.Sprite.Components
+{
+    [Event(false)]
+    public sealed class SpriteComponent : Entitas.IComponent
+    {
+        public string path;
+    }
+}
--- src/Services/Unity/UnityView.cs
+++ src/Services/Unity/UnityView.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 
 namespace btcp.ECS.src.Services.Unity
 {
-	public class UnityView : MonoBehaviour, IView, IPositionListener, IDestroyedListener {
+	public class UnityView : MonoBehaviour, IView, IPositionListener, IDestroyedListener, ISpriteListener {
 
 		void IView.Link(IEntity entity, IContext context)
 		{
 			var e = (GameEntity) entity;
 			e.AddPositionListener(this);
 			e.AddDestroyedListener(this);
+			e.AddSpriteListener(this);
 
-			gameObject.Link(e, context);
-
+			//Automatically add ECS Components if Unity Component present
 			if (e.hasPosition)
 			{
 				var pos = e.position.value;
@@ -24,6 +24,18 @@ namespace btcp.ECS.src.Services.Unity
 			{
 				e.AddPosition(transform.localPosition);
 			}
+
+			if (e.hasSprite == false)
+			{
+				var sr = GetComponent<SpriteRenderer>();
+				if (sr)
+				{
+					e.AddSprite(sr.sprite.name);
+				}
+			}
+			//----------------------------------------
+			
+			gameObject.Link(e, context);
 		}
 
 		public void OnPosition(GameEntity entity, Vector2 value)
@@ -38,5 +50,12 @@ namespace btcp.ECS.src.Services.Unity
 			Destroy(gameObject);
 		}
 
+		public void OnSprite(GameEntity entity, string path)
+		{
+			Debug.Log("On Sprite " + path);
+			var sr = gameObject.GetComponent<SpriteRenderer>();
+			if (sr == null) sr = gameObject.AddComponent<SpriteRenderer>();
+			sr.sprite = Resources.Load<Sprite>(path);
+		}
 	}
 }
